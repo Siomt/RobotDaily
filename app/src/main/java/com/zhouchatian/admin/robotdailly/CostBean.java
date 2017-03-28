a3c9d30019446655c2864a0e454f37b9ac67e405
TheSadFrog <909829990@qq.com>
2017-03-28 23:24:45 +0800

    update CostBean

--- app/src/main/java/com/zhouchatian/admin/robotdailly/CostBean.java
+++ app/src/main/java/com/zhouchatian/admin/robotdailly/CostBean.java
@@ -12,4 +12,37 @@ public class CostBean implements Serializable{
     public String costTitle;
     public String costDate;
     public String costMoney;
+
+    public String getCostTitle() {
+        return costTitle;
+    }
+
+    public void setCostTitle(String costTitle) {
+        this.costTitle = costTitle;
+    }
+
+    public String getCostDate() {
+        return costDate;
+    }
+
+    public void setCostDate(String costDate) {
+        this.costDate = costDate;
+    }
+
+    public String getCostMoney() {
+        return costMoney;
+    }
+
+    public void setCostMoney(String costMoney) {
+        this.costMoney = costMoney;
+    }
+
+    @Override
+    public String toString() {
+        return "CostBean{" +
+                "costTitle='" + costTitle + '\'' +
+                ", costDate='" + costDate + '\'' +
+                ", costMoney='" + costMoney + '\'' +
+                '}';
+    }
 }
